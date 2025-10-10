39445fea590e0d98eab4e81983ed17ac3a147cc3
Dragomir Stoychev <dragomirmd@gmail.com>
2025-10-10 16:44:34 +0300

    add Homework TS Basic exercise

--- index.ts
+++ index.ts
@@ -3,8 +3,8 @@ const age: number = 20;
 
 console.log(`Hello! I am ${name}, ${age} years old`);
 
-const num1: number = 15;
-const num2: number = 7;
+let num1: number = 15;
+let num2: number = 7;
 
 const addition: number = num1 + num2;
 const subtraction: number = num1 - num2;
@@ -12,7 +12,7 @@ const subtraction: number = num1 - num2;
 console.log(`Addition: ${num1} + ${num2} = ${addition}`);
 console.log(`Subtraction: ${num1} - ${num2} = ${subtraction}`);
 
-const num: number = 42;
+let num: number = 42;
 const isWithinRange: boolean = num >= 10 && num <= 50;
 
 console.log(`Number ${num} within [10, 50]: ${isWithinRange}`);
